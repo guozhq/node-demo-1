5aa9c55e1652f1348556f3547b461b4c2fd01590
gwo <shxguozhq@qq.com>
2022-05-30 23:19:13 +0800

    修改server.js文件

--- server.js
+++ server.js
@@ -24,12 +24,24 @@ var server = http.createServer(function (request, response) {
     if (path === '/') {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`二哈`)
+        // response.write(`二哈`)
+        response.write(`
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <title>标题</title>
+                <link rel="stylesheet" href="/x">
+            </head>
+            <body>
+            <h1>我是h1,你呢？</h1>
+            </body>
+        </html>
+        `)
         response.end()
     } else if (path === '/x') {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/css;charset=utf-8')
-        response.write(`body{color: red;}`)
+        response.write(`h1{color: red;}`)
         response.end()
     } else {
         response.statusCode = 404
